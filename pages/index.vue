f677a8fdc44ff206a49340ab4879ed9c523f9106
Bruno Mengaldo <brunomengaldo@gmail.com>
2020-04-06 20:40:57 -0300

    INDENT: first indentation try

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        pokedex
-      </h1>
-      <h2 class="subtitle">
-        my sweet pokedex project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container">
+        <div>
+            <logo />
+            <h1 class="title">
+                pokedex
+            </h1>
+            <h2 class="subtitle">
+                my sweet pokedex project
+            </h2>
+            <div class="links">
+                <a
+                    href="https://nuxtjs.org/"
+                    target="_blank"
+                    class="button--green"
+                >
+                    Documentation
+                </a>
+                <a
+                    href="https://github.com/nuxt/nuxt.js"
+                    target="_blank"
+                    class="button--grey"
+                >
+                    GitHub
+                </a>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
-  }
+    components: {
+        Logo
+    }
 }
 </script>
 
