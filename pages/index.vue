f75371dc8b6525dcf3d1aba15ca3dd4459db8ef0
Bruno Mengaldo <brunomengaldo@gmail.com>
2020-04-24 20:34:09 -0300

    LAYOUT: sidebar init

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="container">
         Landing Page :)
+        <logo class="logo" />
     </div>
 </template>
 
 <script>
+import Logo from '@/static/logo.svg'
 export default {
+    components: {
+        Logo
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .container {
-    padding: 12px 18px;
+    // padding: 12px 18px;
+    & > .logo {
+        border: 1px solid red;
+    }
 }
 </style>
